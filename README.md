a10d59c912d023bbf9f5931c6588edbb57ebbbfd
Lars Francke <lars.francke@gmail.com>
2016-08-17 14:10:36 +0200

    Change to use Spark 2.0.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Kafka Spark Streaming Example
 
-An example project that shows how to use Spark Streaming 1.6.2 with Kafka as input and the Confluent Schema Registry using a specific Avro record (not the generic record).
+An example project that shows how to use Spark Streaming 2.0.0 with Kafka as input and the Confluent Schema Registry using a specific Avro record (not the generic record).
 
 This was tested against Kafka 0.10.0.0.
+
+This is the _spark-2_ branch. The _master_ branch contains a version that is using Spark 1.6.2.
--- src/main/java/com/opencore/examples/SparkStreaming.java
+++ src/main/java/com/opencore/examples/SparkStreaming.java
@@ -6,19 +6,19 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import io.confluent.kafka.serializers.KafkaAvroDecoder;
-import kafka.serializer.StringDecoder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.KafkaUtils;
-import scala.Tuple2;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 
 public class SparkStreaming {
 
-  public static void main(String... args) {
+  public static void main(String... args) throws InterruptedException {
     SparkConf conf = new SparkConf();
     conf.setMaster("local[2]");
     conf.setAppName("Spark Streaming Test Java");
@@ -35,7 +35,7 @@ public class SparkStreaming {
   private static void processStream(JavaStreamingContext ssc, JavaSparkContext sc) {
     System.out.println("--> Processing stream");
 
-    Map<String, String> props = new HashMap<>();
+    Map<String, Object> props = new HashMap<>();
     props.put("bootstrap.servers", "localhost:9092");
     props.put("schema.registry.url", "http://localhost:8081");
     props.put("group.id", "spark");
@@ -46,15 +46,14 @@ public class SparkStreaming {
 
     Set<String> topicsSet = new HashSet<>(Collections.singletonList("test"));
 
-    JavaPairInputDStream<String, Object> stream = KafkaUtils.createDirectStream(ssc, String.class, Object.class,
-      StringDecoder.class, KafkaAvroDecoder.class, props, topicsSet);
+    JavaInputDStream<ConsumerRecord<String, Model>> stream = KafkaUtils.createDirectStream(ssc,
+      LocationStrategies.PreferConsistent(), ConsumerStrategies.Subscribe(topicsSet, props));
 
     stream.foreachRDD(rdd -> {
       rdd.foreachPartition(iterator -> {
           while (iterator.hasNext()) {
-            Tuple2<String, Object> next = iterator.next();
-            Model model = (Model) next._2();
-            System.out.println(next._1() + " --> " + model);
+            ConsumerRecord<String, Model> next = iterator.next();
+            System.out.println(next.key() + " --> " + next.value());
           }
         }
       );
